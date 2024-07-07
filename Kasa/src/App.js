9bb30d7580f4fc9fa33518b681ed89de404c1d3e
david san <david.san732@gmail.com>
2024-07-07 18:49:15 +0200

    master

---
+++ Kasa/src/App.js
@@ -0,0 +1,41 @@
+import Home from './pages/home/Home';
+import NotFound from './pages/notFound/NotFound';
+import About from './pages/about/About';
+import Accomodation from './pages/accomodation/Accomodation';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <NotFound />
+	},
+	{
+		path: "/kasa",
+		element: <Home />
+	},
+	{
+		path: "/accomodation/:id",
+		element: <Accomodation />
+	},
+	{
+		path: '/about',
+		element: <About />
+	},
+	{
+		path: "*",
+		element: <NotFound />
+	},
+]);
+	
+
+
+function App() {
+	return (
+		<>
+			<RouterProvider router={router}/>
+		</>
+	);
+}
+
+export default App;
